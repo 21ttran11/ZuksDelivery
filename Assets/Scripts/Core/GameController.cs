5a9c5bd6b8b989496987879e9509337b72fe72b8
21ttran11 <58841978+21ttran11@users.noreply.github.com>
2024-02-29 08:48:40 -0800

    game controller
    replaced interactables manager with game controller

--- Assets/Scripts/Core/GameController.cs
+++ Assets/Scripts/Core/GameController.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GameState { FreeRoam, Dialog, Interact }
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // controller informs game state
+    [SerializeField] PlayerController playerController;
 }
