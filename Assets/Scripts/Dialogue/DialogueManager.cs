6a3cd8aa6ee7a5833c770766913506e741232bef
21ttran11 <58841978+21ttran11@users.noreply.github.com>
2024-02-29 12:15:33 -0800

    updates
    updated dialogue scripts

---
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace DialogueSystem
+{
+    public class DialogueManager : MonoBehaviour
+    {
+        [SerializeField] GameObject dialogBox;
+        [SerializeField] TextMeshProUGUI dialogueText;
+
+        public void ShowDialogue()
+        {
+
+        }
+    }
+}
