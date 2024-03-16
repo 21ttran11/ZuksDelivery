730b195cfb6de18a48c9d838d4d4038749559a43
Rivaroni <michaelrivaroni@gmail.com>
2024-03-15 21:06:29 -0700

    Action Display Script
    Now when u get in a trigger zone if u press the Q key it will display whatever object you set

---
+++ Assets/Scripts/Triggers/Interactions/ActionDisplay.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ActionDisplay : SceneAction
+{
+    public GameObject objectToDisplay;
+    private Vector3 originalScale;
+
+    private void Start()
+    {
+        originalScale = objectToDisplay.transform.localScale;
+
+        objectToDisplay.SetActive(false);
+        objectToDisplay.transform.localScale = Vector3.zero;
+    }
+
+    public override void Interact()
+    {
+        objectToDisplay.SetActive(true);
+        objectToDisplay.transform.DOScale(originalScale, 0.3f).SetEase(Ease.OutBack);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            objectToDisplay.transform.DOScale(Vector3.zero, 0.3f)
+                .SetEase(Ease.InBack)
+                .OnComplete(() => objectToDisplay.SetActive(false));
+        }
+    }
+}
