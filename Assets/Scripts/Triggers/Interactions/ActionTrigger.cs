42bb626cff732d0b3c717befa0047482d4b595e0
Rivaroni <michaelrivaroni@gmail.com>
2024-03-15 20:51:23 -0700

    Dotween library added
    Added dotween library so its easy to code animations also added animation for visual trigger component

--- Assets/Scripts/Triggers/Interactions/ActionTrigger.cs
+++ Assets/Scripts/Triggers/Interactions/ActionTrigger.cs
@@ -1,17 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class ActionTrigger : MonoBehaviour
 {
     [SerializeField] private SceneAction sceneAction = null;
+    private Vector3 originalScale;
+
+    private void Start()
+    {
+        originalScale = transform.localScale;
+        transform.localScale = Vector3.zero;
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
             sceneAction.Interact();
-            this.gameObject.SetActive(false);
-        }
 
+            AnimateDeactivation();
+        }
+    }
+
+    private void OnDisable()
+    {
+        transform.localScale = Vector3.zero;
+    }
+
+    private void AnimateDeactivation()
+    {
+        transform.DOScale(Vector3.zero, 0.5f).SetEase(Ease.InBack).OnComplete(() => gameObject.SetActive(false));
     }
 }
--- Assets/Scripts/Triggers/Interactions/ObjectActivator.cs
+++ Assets/Scripts/Triggers/Interactions/ObjectActivator.cs
@@ -1,16 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class ObjectActivator : MonoBehaviour
 {
+    private Vector3 originalScale;
+    private GameObject childObject;
+
+    private void Start()
+    {
+        childObject = transform.GetChild(0).gameObject;
+        originalScale = childObject.transform.localScale;
+        childObject.SetActive(false);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        transform.GetChild(0).gameObject.SetActive(true);
+        childObject.SetActive(true);
+        childObject.transform.DOScale(originalScale, 0.5f).SetEase(Ease.OutBack);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        transform.GetChild(0).gameObject.SetActive(false);
+        StartCoroutine(DeactivateAfterAnimation());
+    }
+
+    private IEnumerator DeactivateAfterAnimation()
+    {
+        childObject.transform.DOScale(Vector3.zero, 0.5f).SetEase(Ease.InBack);
+
+        yield return new WaitForSeconds(0.5f);
+
+        childObject.SetActive(false);
     }
 }
