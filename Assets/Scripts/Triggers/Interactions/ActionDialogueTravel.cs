65d3dacb28ffc2632471560f09c46ff109a8ea1c
tiffany <21ttran11@gmail.com>
2024-04-20 15:25:10 -0700

    added dialogue to postoffice 2

--- Assets/Scripts/Triggers/Interactions/ActionDialogueTravel.cs
+++ Assets/Scripts/Triggers/Interactions/ActionDialogueTravel.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 
 public class ActionDialogueTravel : SceneAction
 {
+    [SerializeField]
+    private GameObject dialogue;
+
     public override void Interact()
     {
         Debug.Log("Dialogue Action");
         ButtonManager.canTravel = true;
+        dialogue.SetActive(true);
     }
 }
