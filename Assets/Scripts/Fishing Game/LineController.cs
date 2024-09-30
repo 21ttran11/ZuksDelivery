16bde307600fcf2d92691236a5bd933ef5835a38
tiffany <21ttran11@gmail.com>
2024-09-30 10:21:30 -0700

    fishing line update

--- Assets/Scripts/Fishing Game/LineController.cs
+++ Assets/Scripts/Fishing Game/LineController.cs
@@ -5,33 +5,51 @@ using UnityEngine;
 public class LineController : MonoBehaviour
 {
     [SerializeField]
-    private float extendAmount;
+    private float duration = 2.0f;
+
+    private float scaleFactor = 1.0f;
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 currentScale = transform.localScale;
+
         //scaling from one side (bottom half of y)
         //scale y up/down, position moves half as much as scaling
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow))
         {
-
+            StartCoroutine(FishingLineExtend(duration, currentScale));
         }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
 
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //FishingLineShorten(duration, currentScale);
         }
 
 
     }
 
-    private void FishingLineExtend(float extendAmosunt)
+    private void FishingLineShorten(float duration, Vector2 currentScale)
     {
-        
+
     }
 
-    private void FishingLineShorten(float extendAmount)
+    private IEnumerator FishingLineExtend(float duration, Vector2 currentScale)
     {
+        var elapsed = 0f;
+        var startScale = transform.localScale;
+        currentScale.y += scaleFactor;
+        var endScale = currentScale;
 
+        while(elapsed < duration)
+        {
+            var t = elapsed / duration;
+            transform.localScale = Vector2.Lerp(startScale, endScale, t);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localScale = endScale;
     }
 
 }
