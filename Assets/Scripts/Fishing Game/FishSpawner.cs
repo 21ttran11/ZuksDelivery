13e12ef78e6942aed27fc3249b625509c6041fde
tiffany <21ttran11@gmail.com>
2025-01-02 15:02:05 -0800

    fish manager

--- Assets/Scripts/Fishing Game/FishSpawner.cs
+++ Assets/Scripts/Fishing Game/FishSpawner.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class FishSpawner : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject[] fishSpawners;
+
+    [SerializeField]
+    private GameObject[] fish;
+
+    [SerializeField]
+    private int timeBetweenFish;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,26 @@ public class FishSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        StartCoroutine(Wait());
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(timeBetweenFish);
+        SpawnFish();
+    }
+
+    private void SpawnFish()
+    {
+        int spawnerIndex = Random.Range(0, fishSpawners.Length);
+        GameObject selectedFish = SelectFish();
+        Instantiate(selectedFish, fishSpawners[spawnerIndex].transform); 
+        Debug.Log("Fish Spawned");
+    }
+
+    private GameObject SelectFish()
+    {
+        int fishIndex = Random.Range(0, fish.Length);
+        return fish[fishIndex];
     }
 }
