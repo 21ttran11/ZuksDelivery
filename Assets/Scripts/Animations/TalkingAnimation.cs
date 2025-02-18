57187938d7e96adcd23189dcb2b879cf4ce77802
21ttran11 <58841978+21ttran11@users.noreply.github.com>
2025-02-17 21:14:24 -0800

    Set up talking animation

--- Assets/Scripts/Animations/TalkingAnimation.cs
+++ Assets/Scripts/Animations/TalkingAnimation.cs
@@ -1,52 +1,29 @@
 using System.Collections;
 using UnityEngine;
+using Yarn.Unity;
 
 public class RaccoonAnimation : MonoBehaviour
 {
     private Animator animator;
-    private Coroutine talkingCoroutine;
-    public float talkingDuration = 5f; // Duration for talking in seconds.
 
     void Start()
     {
         animator = GetComponent<Animator>();
     }
 
+    [YarnCommand("Talking")]
     public void PlayTalkingAnimation()
     {
         animator.SetBool("Talking", true);
-
-        // If there's already a talking coroutine, stop it before starting a new one.
-        if (talkingCoroutine != null)
-        {
-            StopCoroutine(talkingCoroutine);
-        }
-
-        talkingCoroutine = StartCoroutine(EndTalkingAnimationAfterTime(talkingDuration));
     }
 
-    private IEnumerator EndTalkingAnimationAfterTime(float duration)
-    {
-        // Wait for the specified duration.
-        yield return new WaitForSeconds(duration);
 
-        // Stop the talking animation.
-        EndTalkingAnimation();
-    }
-
+    [YarnCommand("DoneTalking")]
     public void EndTalkingAnimation()
     {
-        // Ensure that we don't stop the animation if it's already stopped.
         if (animator.GetBool("Talking"))
         {
             animator.SetBool("Talking", false);
-        }
-
-        // Optionally, you can also stop the coroutine here if you want to ensure it doesn't run again.
-        if (talkingCoroutine != null)
-        {
-            StopCoroutine(talkingCoroutine);
-            talkingCoroutine = null;
         }
     }
 }
